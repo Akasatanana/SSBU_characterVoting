e49f921d7a78a46fb54aa1b0e7655ffc7c1fee8e
akasata nana <akasantanana0894@gmail.com>
2022-07-22 03:10:56 +0900

    DBの反映確認

--- htmls/voting.php
+++ htmls/voting.php
@@ -10,10 +10,12 @@
     <strong class = "title2_font">キャラクターのパラメータを入力してね</strong>
     
     <?php
+      $charaname =  explode(".", explode("/", $_POST["charaurl"])[3])[0];
+      
       $db_host = 'localhost';
       $db_user = 'root';
       $db_password = 'root';
-      $db_db = 'information_schema';
+      $db_db = 'SSBU_charaVoting';
     
       $mysqli = @new mysqli(
         $db_host,
@@ -21,9 +23,26 @@
         $db_password,
         $db_db
       );
+
+      if ($mysqli->connect_error) {
+        echo $mysqli->connect_error;
+        exit();
+    } else {
+        $mysqli->set_charset("utf8");
+    }
+
+    $sql = "SELECT * FROM characterName";
+    if ($result = $mysqli->query($sql)) {
+        // 連想配列を取得
+        while ($row = $result->fetch_assoc()) {
+            echo $row["id"] . $row["name"] . "<br>";
+        }
+        // 結果セットを閉じる
+        $result->close();
+    }
+
+
       $mysqli->close();
-      $charaname =  explode(".", explode("/", $_POST["charaurl"])[3])[0];
-      echo $charaname;
     ?>
 </body>
 </html>
