a7a0a41aea7fb555f83f81894db75154a842851d
akasata nana <akasantanana0894@gmail.com>
2022-07-24 20:14:06 +0900

    usernameページの変更

--- htmls/username.php
+++ htmls/username.php
@@ -15,7 +15,7 @@
     <div class = "username-containter">
         <h1 class = "username-headText">名前を入力してね</h1>
         <form method="get" action="charaSelect.php" target="_blank">
-            <input class="username-textbox" type="text" name="username" autocomplete="off" maxlength="60">
+            <input class="username-textbox" type="text" name="username" autocomplete="off" maxlength="60"><br>
             <button type = "submit"><i class="fa-solid fa-right"></i></button>
         </form>
     </div>
