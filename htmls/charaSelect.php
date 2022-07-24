172ac05543ff4b0c16715a91909cf3d290628f56
akasata nana <akasantanana0894@gmail.com>
2022-07-25 02:57:10 +0900

    新規ユーザ登録のテスト

--- htmls/charaSelect.php
+++ htmls/charaSelect.php
@@ -1,3 +1,53 @@
+<?php
+    $existsUsername = false;
+    $notexistUsername = false;
+    $incorrectPassword = false;
+
+    $db_host = 'mysql630.db.sakura.ne.jp';
+    $db_user = 'ssbu-charavoting';
+    $db_password = 'mkai0894';
+    $db_db = 'ssbu-charavoting_chara-voting';
+
+    $mysqli = @new mysqli(
+        $db_host,
+        $db_user,
+        $db_password,
+        $db_db
+    );
+
+    if ($mysqli->connect_error) {
+        echo $mysqli->connect_error;
+        exit();
+    } else {
+        $mysqli->set_charset("utf8");
+    }
+
+    if ($_POST["usertype"] == "new") {
+        $sql = "SELECT username FROM username WHERE username = ?";
+        if ($result = $mysqli->prepare($sql)) {
+            $result->bind_param("s", $_POST['username']);
+            $result->execute();
+
+            $result->store_result(); // これ忘れるとnum_rowsは0
+            $rows = $result->num_rows;
+            if ($rows == 0) {
+                $stmt = $mysqli->prepare('INSERT INTO username (
+                            username, paswword
+                          ) VALUES (
+                            ?, ?
+                          )');
+                // (5)登録するデータをセット
+                $stmt->bind_param('ss', $_POST['username'], $_POST['password']);
+                // (6)登録実行
+                $stmt->execute();
+                $stmt->close();
+            } else {
+                $existsUsername = true;
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,18 +57,26 @@
 </head>
 
 <body>
-    <strong class = "title2_font">キャラクターを選択してね</strong>
-    <div class="imagezone">
-        <form method="POST" action="voting.php">
-            <?php
-                $images = glob('../charaImages//*png');
-                foreach($images as $v) {
-                    echo '<button type = "submit" name = "charaurl" value = "' , $v , '" onclick = "location.href= "voting.php"" class = "nobordered_button">
-                            <img src="' , $v , '" alt="" loading="lazy" class="image-vw">
-                        </button>';
-                }
-            ?>
-        </form>
-    </div>
+    <?php if($existsUsername){ ?> <!--新規ユーザで登録不可能-->
+        <strong class = "title2_font">このユーザ名は既に登録されています</strong>
+    <?php } else if($notexistUsername){ ?> <!--既存ユーザでユーザ名が存在しない-->
+    
+    <?php } else if($notexistUsername){ ?> <!--既存ユーザでパスワードが異なる-->
+
+    <?php }else{ ?><!--正常な処理-->
+        <strong class = "title2_font">キャラクターを選択してね</strong>
+        <div class="imagezone">
+            <form method="POST" action="voting.php">
+                <?php
+                    $images = glob('../images/charaImages//*png');
+                    foreach($images as $v) {
+                        echo '<button type = "submit" name = "charaurl" value = "' , $v , '" onclick = "location.href= "voting.php"" class = "nobordered_button">
+                                <img src="' , $v , '" alt="" loading="lazy" class="image-vw">
+                            </button>';
+                    }
+                ?>
+            </form>
+        </div>
+    <?php } ?>
 </body>
 </html>
--- htmls/username.php
+++ htmls/username.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
-    <meta name = "viewport" content = "width = device-width, initial-scale = 1">
+    <meta name="viewport" content="width = device-width, initial-scale = 1">
     <title>SSBU_charaVoting</title>
     <!--cssの初期化用，必ず先頭に-->
     <link rel="stylesheet" href="https://unpkg.com/destyle.css@3.0.2/destyle.min.css">
@@ -16,79 +17,19 @@
 </head>
 
 <body>
-    <div class = "username-containter1">
-        <h1 class = "username-headText">名前とパスワードを入力</h1>
-        <form method="post" action="" target="_blank">
-                <div class = "usertype-select">
-                    <input type="radio" name = "usertype" value = "new" checked> 新規ユーザ
-                    <input type="radio" name = "usertype" value = "registered"> 登録済み<br>
-                </div>
+    <div class="username-containter1">
+        <h1 class="username-headText">名前とパスワードを入力</h1>
+        <form method="post" action="charaSelect.php" target="_blank">
+            <div class="usertype-select">
+                <input type="radio" name="usertype" value="new" checked> 新規ユーザ
+                <input type="radio" name="usertype" value="registered"> 登録済み<br>
+            </div>
             <input class="username-textbox" type="text" name="username" placeholder="ユーザ名" autocomplete="off" maxlength="60"><br>
             <input class="username-textbox" type="text" name="password" placeholder="パスワード" autocomplete="off" maxlength="60"><br>
-            <button class = "username-submit-button" type="submit">キャラ選択へ</button>
+            <button class="username-submit-button" type="submit">キャラ選択へ</button>
 
         </form>
     </div>
-    <script>
-        var existusername = JSON.parse(<?php echo $existsUsername_json ?>);
-        if (existusername){
-            alert("このユーザ名は既に登録されています");
-        }else{
-            alert("登録完了");
-        }
-    </script>
-    <?php
-    $existsUsername = false;
-        if(isset($_POST["submit"])){
-            $db_host = 'mysql630.db.sakura.ne.jp';
-            $db_user = 'ssbu-charavoting';
-            $db_password = 'mkai0894';
-            $db_db = 'ssbu-charavoting_chara-voting';
-        
-            $mysqli = @new mysqli(
-            $db_host,
-            $db_user,
-            $db_password,
-            $db_db
-            );
-
-            if ($mysqli->connect_error) {
-                echo $mysqli->connect_error;
-                exit();
-            } else {
-                $mysqli->set_charset("utf8");
-            }
-
-            if($_POST["usertype"] == "new"){
-
-                $sql = "SELECT username FROM username WHERE username = ?";
-                if ($result = $mysqli->prepare($sql)) {
-                    $result->bind_param("s", $_POST['username']);
-                    $result->execute();
-
-                    $result->store_result(); // これ忘れるとnum_rowsは0
-                    $rows = $result->num_rows;
-                    if($rows == 0){
-                        $stmt = $mysqli->prepare('INSERT INTO username (
-                            username, paswword
-                          ) VALUES (
-                            ?, ?
-                          )');
-                        // (5)登録するデータをセット
-                        $stmt->bind_param('ss', $_POST['username'], $_POST['password']);
-                        // (6)登録実行
-                        $stmt->execute();
-                        $stmt->close();
-                    }else{
-                        $existsUsername = true;
-                        $existsUsername_json = json_encode($existsUsername);
-                    }
-                }
-
-            }else{
-
-            }
-        }
-    ?>
 </body>
+
 </html>
