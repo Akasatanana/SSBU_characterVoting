40d01a1a2bfb0fe4b46ac299806555e826f89374
akasata nana <akasantanana0894@gmail.com>
2022-07-25 03:20:09 +0900

    postのテスト

--- htmls/charaSelect.php
+++ htmls/charaSelect.php
@@ -58,10 +58,22 @@
 </head>
 
 <body>
-    <?php 
-        echo $existsUsername."<br>";
-        echo $_POST["username"]."<br>";
-        echo $_POST["password"]."<br>";
-    ?>
+    <?php if($existsUsername){ ?> <!--新規ユーザで登録不可能-->
+        <strong class = "title2_font">このユーザ名は既に登録されています</strong>
+    <?php }else{ ?><!--正常な処理-->
+        <strong class = "title2_font">キャラクターを選択してね</strong>
+        <div class="imagezone">
+            <form method="POST" action="voting.php">
+                <?php
+                    $images = glob('../images/charaImages//*png');
+                    foreach($images as $v) {
+                        echo '<button type = "submit" name = "charaurl" value = "' , $v , '" onclick = "location.href= "voting.php"" class = "nobordered_button">
+                                <img src="' , $v , '" alt="" loading="lazy" class="image-vw">
+                            </button>';
+                    }
+                ?>
+            </form>
+        </div>
+    <?php } ?>
 </body>
 </html>
