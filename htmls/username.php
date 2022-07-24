cbd8de2200a5b3c9620dbf58dce481e072a28fd1
akasata nana <akasantanana0894@gmail.com>
2022-07-24 19:58:10 +0900

    usernameページの変更

--- htmls/username.php
+++ htmls/username.php
@@ -3,18 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>SSBU_charaVoting</title>
+    <link rel="stylesheet" href="https://unpkg.com/destyle.css@3.0.2/destyle.min.css">
     <link rel="stylesheet" href="../csss/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e435c5f384.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div id="containter">
-        <h1>名前を入力してね</h1>
+    <div class = "username-containter">
+        <h1 class = "username-headText">名前を入力してね</h1>
         <form method="get" action="charaSelect.php" target="_blank">
-            <input class="textbox" type="text" name="q" placeholder="ユーザ名" autocomplete="off" maxlength="60">
+            <input class="username-textbox" type="text" name="username" autocomplete="off" maxlength="60">
+            <button type = "submit"><i class="fa-solid fa-right"></i></button>
         </form>
-</div>
+    </div>
 </body>
 </html>
